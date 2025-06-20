1b2869989f2da123be95a0b94099bae1209bd25b
Ethan MacLeod <ethandavidfrancis@gmail.com>
2025-06-20 21:47:23 +1200

    feat!: add basic trpc server

---
+++ apps/api/src/main.ts
@@ -0,0 +1,30 @@
+import {
+    fastifyTRPCPlugin,
+    FastifyTRPCPluginOptions,
+} from '@trpc/server/adapters/fastify';
+import fastify from 'fastify';
+import { createContext } from './context';
+import { appRouter, type AppRouter } from './router';
+
+const server = fastify({ maxParamLength: 5000 });
+
+server.register(fastifyTRPCPlugin, {
+  prefix: '/trpc',
+  trpcOptions: {
+    router: appRouter,
+    createContext,
+    onError({ path, error }) {
+      console.error(`Error in tRPC handler on path '${path}':`, error);
+    },
+  } satisfies FastifyTRPCPluginOptions<AppRouter>['trpcOptions'],
+});
+
+
+(async () => {
+  try {
+    await server.listen({ port: 3000 });
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+})();
